4f8cc6795a4a31f6c6db7b4b0382e0db2f3089a8
gujie1216933842 <gujientsy@163.com>
2018-10-07 16:22:52 +0800

    flask_restful_api

--- app/api/v1/book.py
+++ app/api/v1/book.py
@@ -4,11 +4,11 @@ from app.libs.redprint import Redprint
 api = Redprint('book')
 
 
-@api.route('/get')
+@api.route('', methods=['GET'])
 def get_book():
     return 'get book'
 
 
-@api.route('/create')
+@api.route('', methods=['POST'])
 def create_book():
     return 'create book'
