fbabe940db31922afdfd0b9ab6481570f823916c
gujie <jie.j.gu@rrd.com>
2018-10-12 17:09:18 +0800

    flask_api

--- app/validators/base.py
+++ app/validators/base.py
@@ -7,8 +7,10 @@ from app.libs.error_code import ParameterException
 class BaseForm(Form):
     def __init__(self):
         data = request.get_json(silent=True)
+        print(data)
         args = request.args.to_dict()
-
+        print(args)
+        '''super(BaseForm,self) 首先找到 BaseForm 的父类（就是类 Form），然后把类B的对象 BaseForm 转换为类 Form 的对象'''
         super(BaseForm, self).__init__(data=data, **args)
 
     def validate_for_api(self):
--- test/01_test.py
+++ test/01_test.py
@@ -2,7 +2,7 @@ import  requests
 
 import json
 url = 'http://localhost:5000/v1/client/register'
-payload = {'nickname':'xiaoming','email': 'data','password':'123456ssddds','type':101}
+payload = {'account':'aaaa','nickname':'xiaoming','email': 'data','password':'123456ssddds','type':101}
 headers = {'content-type': 'application/json'}
 
 r = requests.post(url, data=json.dumps(payload), headers=headers)
