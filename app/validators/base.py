83d05b771bceca961bdf0c5efd5edc1ea425dd62
gujie <jie.j.gu@rrd.com>
2018-10-22 15:27:45 +0800

    aa

--- app/validators/base.py
+++ app/validators/base.py
@@ -1,23 +1,23 @@
+"""
+ Created by 七月 on 2018/5/12.
+"""
 from flask import request
-
 from wtforms import Form
+
 from app.libs.error_code import ParameterException
+
+__author__ = '七月'
 
 
 class BaseForm(Form):
     def __init__(self):
         data = request.get_json(silent=True)
-        print(data)
         args = request.args.to_dict()
-        print(args)
-        '''super(BaseForm,self) 首先找到 BaseForm 的父类（就是类 Form），然后把类B的对象 BaseForm 转换为类 Form 的对象'''
         super(BaseForm, self).__init__(data=data, **args)
 
     def validate_for_api(self):
-        print('ccccccccc')
         valid = super(BaseForm, self).validate()
-        # print(valid)
-        print('ddddddddd')
         if not valid:
+            # form errors
             raise ParameterException(msg=self.errors)
         return self
